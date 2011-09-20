d3cdf0960a52cdea46096e2da7bb21c636bc0c10
andrie.de.vries <andrie.de.vries@pentalibra.com>
2011-09-20 18:03:40 +0100

    Removed HMisc dependency.

--- R/latex.R
+++ R/latex.R
@@ -1,15 +1,8 @@
-################################################################################
-###  Define Latex functions
-################################################################################
+#  Define Latex functions
 
-#' Prints a string to Latex, dealing with special characters
+#' Prints a string to Latex, dealing with special characters.
 #' 
-#' Translates particular items in character strings to LaTeX format, 
-#' e.g., makes a^2 = a\$^2\$ for superscript within variable labels. 
-#' LaTeX names of greek letters (e.g., "alpha") will have backslashes 
-#' added if greek==TRUE. Math mode is inserted as needed. 
-#' latexTranslate assumes that input text always has matches, 
-#' e.g. [) [] (] (), and that surrounding by \$\$ is OK. 
+#' Translates particular items in character strings to LaTeX format, e.g., makes a^2 = a\$^2\$ for superscript within variable labels. LaTeX names of greek letters (e.g., "alpha") will have backslashes added if greek==TRUE. Math mode is inserted as needed. \code{latexTranslate} assumes that input text always has matches, e.g. [) [] (] (), and that surrounding by \$\$ is OK. 
 #' @note This code is copied from Hmisc.  Hmisc used to export this function.
 #' @param object A string containing question text
 #' @param inn specify additional input strings over the usual defaults 
@@ -18,7 +11,7 @@
 #' @param greek set to TRUE to have latexTranslate put names for greek letters
 #' in math mode and add backslashes
 #' @param ... ignored 
-#' @keywords internal
+#' @keywords Internal
 latexTranslate <- function (object, inn = NULL, out = NULL, pb = FALSE, greek = FALSE, 
 		...) 
 {
@@ -68,18 +61,61 @@ latexTranslate <- function (object, inn = NULL, out = NULL, pb = FALSE, greek = 
 }
 
 
-##' Prints a string to Latex, dealing with special characters
-##'
-##' @param x A string containing question text
-##' @keywords internal
-#latex_question <- function(x){
-##  cat("\\section{", x, "}\n\n", sep="")
-#	inn <- "&"
-#	out <- "\\&"
-#	latexTranslate(
-#					paste("\n\\section{", x, "}\n\n", sep=""), 
-#					inn=inn, 
-#					out=out)
-#}
+#' String editing function, adapted from Hmisc.
+#' 
+#' @param text a vector of character strings for sedit, substring2, substring2<- or a single character string for substring.location, replace.substring.wild.  
+#' @param from a vector of character strings to translate from, for sedit. A single asterisk wild card, meaning allow any sequence of characters (subject to the test function, if any) in place of the "*". An element of from may begin with "^" to force the match to begin at the beginning of text, and an element of from can end with "$" to force the match to end at the end of text.  
+#' @param to a vector of character strings to translate to, for sedit. If a corresponding element in from had an "*", the element in to may also have an "*". Only single asterisks are allowed. If to is not the same length as from, the rep function is used to make it the same length.
+#' @param test a function of a vector of character strings returning a logical vector whose elements are TRUE or FALSE according to whether that string element qualifies as the wild card string for sedit, replace.substring.wild  
+#' @param wild.literal set to TRUE to not treat asterisks as wild cards and to not look for "^" or "$" in old  
+#' @param keywords Internal
+#' @note This code is copied from Hmisc
+sedit <- function (text, from, to, test = NULL, wild.literal = FALSE) 
+{
+  to <- rep(to, length = length(from))
+  for (i in 1:length(text)) {
+    s <- text[i]
+    if (length(s)) 
+      for (j in 1:length(from)) {
+        old <- from[j]
+        front <- back <- FALSE
+        if (!wild.literal) {
+          if (substring(old, 1, 1) == "^") {
+            front <- TRUE
+            old <- substring(old, 2)
+          }
+          if (substring(old, nchar(old)) == "$") {
+            back <- TRUE
+            old <- substring(old, 1, nchar(old) - 1)
+          }
+        }
+        new <- to[j]
+        lold <- nchar(old)
+        if (lold > nchar(s)) 
+          next
+        ex.old <- substring(old, 1:lold, 1:lold)
+        if (!wild.literal && any(ex.old == "*")) 
+          s <- replace.substring.wild(s, old, new, test = test, 
+              front = front, back = back)
+        else {
+          l.s <- nchar(s)
+          is <- 1:(l.s - lold + 1)
+          if (front) 
+            is <- 1
+          ie <- is + lold - 1
+          if (back) 
+            ie <- l.s
+          ss <- substring(s, is, ie)
+          k <- ss == old
+          if (!any(k)) 
+            next
+          k <- is[k]
+          substring2(s, k, k + lold - 1) <- new
+        }
+      }
+    text[i] <- s
+  }
+  text
+}
 
 
